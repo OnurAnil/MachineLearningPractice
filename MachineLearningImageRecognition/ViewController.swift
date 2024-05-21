e529424c124258c929726f1f1851017a23aa80ba
Onur Anil <onuranil749@gmail.com>
2024-05-21 16:23:01 +0300

    Initial Commit

---
+++ MachineLearningImageRecognition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MachineLearningImageRecognition
+//
+//  Created by Onur Anıl on 21.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
